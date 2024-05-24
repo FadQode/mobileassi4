afb475d579a23d02e484a1abafb5fd25d3cf5ca4
Fad <fadhilerdyaqasmal@gmail.com>
2024-05-24 14:39:29 +0700

    init project and stuff

---
+++ app/src/main/java/com/example/fragprac/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.fragprac;
+
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+    FrameLayout frameLayout;
+    TabLayout tabLayout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        frameLayout = (FrameLayout) findViewById(R.id.frameLayout);
+        tabLayout = (TabLayout) findViewById(R.id.tablayout);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, new FormFragment())
+                .addToBackStack(null)
+                .commit();
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+
+                Fragment fragment = null;
+                switch (tab.getPosition()){
+                    case 0:
+                        fragment = new FormFragment();
+                        break;
+                    case 1:
+                        fragment = new ShowFragment();
+                        break;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, fragment).setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+    }
+
+
+
+
+}
